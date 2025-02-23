c7f831aaaf84d7a8ffb86e8f227c07514bf3f287
Nataniel Jayaseelan <63678990+natanielj@users.noreply.github.com>
2025-02-23 01:52:00 -0800

    project works template needs to improve

--- biohacks/biohacks/asgi.py
+++ biohacks/biohacks/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for biohacks25 project.
+ASGI config for biohacks project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "biohacks25.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'biohacks.settings')
 
 application = get_asgi_application()
